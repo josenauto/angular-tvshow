f950bde02e9949d9aba79082812bbf85bb2ba02c
josenauto <jemilionautoch@gmail.com>
2015-08-04 17:44:57 -0300

    Create the array shows in the controller and display the shows in the index.html

--- main.ctrl.js
+++ main.ctrl.js
@@ -2,4 +2,31 @@ angular.module('app').controller("MainController", function(){
     var vm = this;
     vm.title = 'Angularjs TV Shows';
     vm.searchInput = '';
+    vm.shows = [
+      {
+        title: 'Game of Thrones',
+        year: 2011,
+        favorite: true
+      },
+      {
+        title: 'Walking Dead',
+        year: 2010,
+        favorite: false
+      },
+      {
+        title: 'Firefly',
+        year: 2002,
+        favorite: true
+      },
+      {
+        title: 'Banshee',
+        year: 2013,
+        favorite: true
+      },
+      {
+        title: 'Greys Anatomy',
+        year: 2005,
+        favorite: false
+      }
+    ];
 });
